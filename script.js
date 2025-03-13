e9c4e11d93bec2b7149a4c14d49f3666b58949f9
onedevmaster <jssmeca@hotmail.com>
2025-03-13 17:58:56 -0300

    Segunda alteração do commit

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const imagens = [
+    // Primeira galeria de imagens
+    const imagens1 = [
         "imagens/imagem1.jpg",
         "imagens/imagem2.jpg",
         "imagens/imagem3.jpg",
-	"imagens/imagem4.jpg"
+        "imagens/imagem4.jpg"
     ];
 
-    const galeria = document.getElementById("galeria");
+    const galeria1 = document.getElementById("galeria1");
 
-    imagens.forEach(imagem => {
+    imagens1.forEach(imagem => {
         const imgElement = document.createElement("img");
         imgElement.src = imagem;
-        imgElement.alt = "Imagem da galeria";
-        galeria.appendChild(imgElement);
+        imgElement.alt = "Imagem da galeria 1";
+        galeria1.appendChild(imgElement);
+    });
+
+    // Segunda galeria de imagens
+    const imagens2 = [
+        "imagens/LateralEsquerda02.jpg",
+        "imagens/LateralDireita03.jpg",
+        "imagens/Frontal01.jpg",
+	"imagens/imagem4.jpg"
+
+    ];
+
+    const galeria2 = document.getElementById("galeria2");
+
+    imagens2.forEach(imagem => {
+        const imgElement = document.createElement("img");
+        imgElement.src = imagem;
+        imgElement.alt = "Imagem da galeria 2";
+        galeria2.appendChild(imgElement);
     });
 });
